0cc809426c1ffc86b4dde24a0bdcb745ebabdda3
haydentcoleman <107648037+haydentcoleman@users.noreply.github.com>
2022-07-11 13:21:27 -0500

    Update signin.js

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -55,25 +55,9 @@ export default function Page() {
               <Button styleType="secondary" href="/signin">
                 SIGN IN
               </Button>
-              <Button styleType="primary" href="/about">
-                LEARN MORE
-              </Button>
             </div>
           </section>
-          <section className="cta">
-            <CTA
-              Button={() => (
-                <Button href="/posts">
-                  Get Started <FaArrowRight style={{ marginLeft: `1rem` }} />
-                </Button>
-              )}
-            >
-              <span>
-                Learn about Core Web Vitals and how Atlas can help you reach
-                your most demanding speed and user experience requirements.
-              </span>
-            </CTA>
-          </section>
+          
           <section className={styles.posts}>
             <Heading className={styles.heading} level="h2">
               Latest Posts
