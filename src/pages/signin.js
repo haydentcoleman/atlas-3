2694ba7a827d7ea8b847bbd98773bb6d1b38764c
haydentcoleman <107648037+haydentcoleman@users.noreply.github.com>
2022-07-11 12:04:37 -0500

    Update signin.js

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,54 +1,75 @@
-function setFormMessage(formElement, type, message) {
-    const messageElement = formElement.querySelector(".form__message");
+import { getNextStaticProps } from '@faustjs/next';
+import React from 'react';
+import { client } from 'client';
+import { FaArrowRight } from 'react-icons/fa';
+import {
+  Posts,
+  Header,
+  Footer,
+  EntryHeader,
+  Main,
+  Button,
+  Heading,
+  CTA,
+  Testimonials,
+  SEO,
+} from 'components';
+import styles from 'styles/pages/_Home.module.scss';
+import { pageTitle } from 'utils';
 
-    messageElement.textContent = message;
-    messageElement.classList.remove("form__message--success", "form__message--error");
-    messageElement.classList.add(`form__message--${type}`);
+const postsPerPage = 3;
+
+export default function Page() {
+  const { useQuery, usePosts } = client;
+  const generalSettings = useQuery().generalSettings;
+  const posts = usePosts({
+    first: postsPerPage,
+    where: {
+      categoryName: 'uncategorized',
+    },
+  });
+  const testimonials = useQuery().testimonials();
+  const mainBanner = {
+    sourceUrl: '/static/banner.jpeg',
+    mediaDetails: { width: 1200, height: 600 },
+    altText: 'Portfolio Banner',
+  };
+
+  return (
+    <>
+      <SEO
+        title={pageTitle(generalSettings)}
+        imageUrl={mainBanner?.sourceUrl}
+      />
+
+      <Header />
+
+      <Main className={styles.home}>
+        <EntryHeader image={mainBanner} />
+        <div className="container">
+          <section className="hero text-center">
+            <Heading className={styles.heading} level="h1">
+             Sign In Below
+            </Heading>
+            
+          <section className={styles.posts}>
+            <Heading className={styles.heading} level="h2">
+              Sign In Below
+            </Heading>
+            <Posts posts={posts?.nodes} id="posts-list" />
+          </section>
+          
+        </div>
+      </Main>
+
+      <Footer />
+    </>
+  );
 }
 
-function setInputError(inputElement, message) {
-    inputElement.classList.add("form__input--error");
-    inputElement.parentElement.querySelector(".form__input-error-message").textContent = message;
+export async function getStaticProps(context) {
+  return getNextStaticProps(context, {
+    Page,
+    client,
+  });
 }
-
-function clearInputError(inputElement) {
-    inputElement.classList.remove("form__input--error");
-    inputElement.parentElement.querySelector(".form__input-error-message").textContent = "";
-}
-
-document.addEventListener("DOMContentLoaded", () => {
-    const loginForm = document.querySelector("#login");
-    const createAccountForm = document.querySelector("#createAccount");
-
-    document.querySelector("#linkCreateAccount").addEventListener("click", e => {
-        e.preventDefault();
-        loginForm.classList.add("form--hidden");
-        createAccountForm.classList.remove("form--hidden");
-    });
-
-    document.querySelector("#linkLogin").addEventListener("click", e => {
-        e.preventDefault();
-        loginForm.classList.remove("form--hidden");
-        createAccountForm.classList.add("form--hidden");
-    });
-
-    loginForm.addEventListener("submit", e => {
-        e.preventDefault();
-
-        // Perform your AJAX/Fetch login
-
-        setFormMessage(loginForm, "error", "Invalid username/password combination");
-    });
-
-    document.querySelectorAll(".form__input").forEach(inputElement => {
-        inputElement.addEventListener("blur", e => {
-            if (e.target.id === "signupUsername" && e.target.value.length > 0 && e.target.value.length < 10) {
-                setInputError(inputElement, "Username must be at least 10 characters in length");
-            }
-        });
-
-        inputElement.addEventListener("input", e => {
-            clearInputError(inputElement);
-        });
-    });
-});
