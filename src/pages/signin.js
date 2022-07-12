7c5897d74728ba245b225cf9e45763efc303ebb3
haydentcoleman <107648037+haydentcoleman@users.noreply.github.com>
2022-07-12 11:23:28 -0500

    Update signin.js

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -34,7 +34,7 @@ export default function Page() {
       <Main className={styles.box}>
         
         <div className="box">
-            <Main image={mainBanner} />
+            <box image={mainBanner} />
             <section className="hero text-center">
               <Heading className={styles.heading} level="h1">
                 SIGN IN
