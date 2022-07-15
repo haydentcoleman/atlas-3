99bfd2a332d5f47f186bf10f05d4a5b17c8ad397
haydentcoleman <107648037+haydentcoleman@users.noreply.github.com>
2022-07-15 12:18:41 -0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -31,8 +31,8 @@ export default function Page() {
       
       
      
-       <Main className={styles.body} background={mainBanner}>
-         <div className="body">
+       <Main className={styles.body} >
+         <div className="body" background={mainBanner}>
           
             <Main className={styles.box}>
               <div className="box">
